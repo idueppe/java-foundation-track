1d46df1d2c8de8d6ea2083e49b13ec9ad8f0928c
idueppe <ingo.dueppe@dueppe.com>
2013-12-03 12:00:10 +0100

    Fixed jpa problem

--- vehicle-jpa/src/main/java/com/lsy/vehicle/domain/Seat.java
+++ vehicle-jpa/src/main/java/com/lsy/vehicle/domain/Seat.java
@@ -1,17 +1,21 @@
 package com.lsy.vehicle.domain;
 
+import javax.persistence.Embeddable;
 
+@Embeddable
+public class Seat
+{
 
-public class Seat {
-    
-    private String type;
+	private String	type;
 
-    public String getType() {
-        return type;
-    }
+	public String getType()
+	{
+		return type;
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
-    
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+
 }
--- vehicle-jpa/src/main/java/com/lsy/vehicle/domain/Vehicle.java
+++ vehicle-jpa/src/main/java/com/lsy/vehicle/domain/Vehicle.java
@@ -14,119 +14,142 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Version;
 
 @Entity
-@NamedQueries(value={
-    @NamedQuery(name="vehicleFindAll", query="SELECT v FROM Vehicle v"),
-    @NamedQuery(name="vehicleFindByManufacturerName", query="SELECT v FROM Vehicle v WHERE v.manufacturer.name = :name"),
-    @NamedQuery(name="vehicleByEngineType", query="SELECT v FROM Vehicle v WHERE v.engine.type = :engineType"),
-    @NamedQuery(name="vehicleFindCheapest", query="SELECT v FROM Vehicle v WHERE v.nettoPrice = (SELECT min(v.nettoPrice) FROM Vehicle v)")
-    
+@NamedQueries(value = {
+		@NamedQuery(name = "vehicleFindAll", query = "SELECT v FROM Vehicle v"),
+		@NamedQuery(name = "vehicleFindByManufacturerName", query = "SELECT v FROM Vehicle v WHERE v.manufacturer.name = :name"),
+		@NamedQuery(name = "vehicleByEngineType", query = "SELECT v FROM Vehicle v WHERE v.engine.type = :engineType"),
+		@NamedQuery(name = "vehicleFindCheapest", query = "SELECT v FROM Vehicle v WHERE v.nettoPrice = (SELECT min(v.nettoPrice) FROM Vehicle v)")
+
 })
-public class Vehicle {
+public class Vehicle
+{
 
-    @Id
-    @GeneratedValue
-    private Long id;
+	@Id
+	@GeneratedValue
+	private Long			id;
 
-    @ManyToOne()
-    private Manufacturer manufacturer;
-    
-    @ManyToOne(cascade=CascadeType.PERSIST)
-    private Engine engine;
-    
-    private String model;
-    private Double nettoPrice;
-    private Date constructionDate;
+	@ManyToOne()
+	private Manufacturer	manufacturer;
 
-    private int kilometres;
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	private Engine			engine;
 
-    private URL smallImageURL;
-    private URL normalImageURL;
-    
-    @Embedded
-    private Seat seat;
-    
-    @Version 
-    private long version;
+	private String			model;
 
-    public Long getId() {
-        return id;
-    }
+	private Double			nettoPrice;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-    
-    public String getModel() {
-        return model;
-    }
+	private Date			constructionDate;
 
-    public void setModel(String model) {
-        this.model = model;
-    }
+	private int				kilometres;
 
-    public Double getNettoPrice() {
-        return nettoPrice;
-    }
+	private URL				smallImageURL;
 
-    public void setNettoPrice(Double nettoPrice) {
-        this.nettoPrice = nettoPrice;
-    }
+	private URL				normalImageURL;
 
-    public Date getConstructionDate() {
-        return constructionDate;
-    }
+	@Embedded
+	private Seat			seat;
 
-    public void setConstructionDate(Date constructionDate) {
-        this.constructionDate = constructionDate;
-    }
+	@Version
+	private long			version;
 
-    public int getKilometres() {
-        return kilometres;
-    }
+	public Long getId()
+	{
+		return id;
+	}
 
-    public void setKilometres(int kilometres) {
-        this.kilometres = kilometres;
-    }
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
 
-    public URL getSmallImageURL() {
-        return smallImageURL;
-    }
+	public String getModel()
+	{
+		return model;
+	}
 
-    public void setSmallImageURL(URL smallImageURL) {
-        this.smallImageURL = smallImageURL;
-    }
+	public void setModel(String model)
+	{
+		this.model = model;
+	}
 
-    public URL getNormalImageURL() {
-        return normalImageURL;
-    }
+	public Double getNettoPrice()
+	{
+		return nettoPrice;
+	}
 
-    public void setNormalImageURL(URL normalImageURL) {
-        this.normalImageURL = normalImageURL;
-    }
+	public void setNettoPrice(Double nettoPrice)
+	{
+		this.nettoPrice = nettoPrice;
+	}
 
-    public Manufacturer getManufacturer() {
-        return manufacturer;
-    }
+	public Date getConstructionDate()
+	{
+		return constructionDate;
+	}
 
-    public void setManufacturer(Manufacturer manufacturer) {
-        this.manufacturer = manufacturer;
-    }
+	public void setConstructionDate(Date constructionDate)
+	{
+		this.constructionDate = constructionDate;
+	}
 
-    public Engine getEngine() {
-        return engine;
-    }
+	public int getKilometres()
+	{
+		return kilometres;
+	}
 
-    public void setEngine(Engine engine) {
-        this.engine = engine;
-    }
+	public void setKilometres(int kilometres)
+	{
+		this.kilometres = kilometres;
+	}
 
-    public Seat getSeat() {
-        return seat;
-    }
+	public URL getSmallImageURL()
+	{
+		return smallImageURL;
+	}
 
-    public void setSeat(Seat seat) {
-        this.seat = seat;
-    }
+	public void setSmallImageURL(URL smallImageURL)
+	{
+		this.smallImageURL = smallImageURL;
+	}
 
+	public URL getNormalImageURL()
+	{
+		return normalImageURL;
+	}
+
+	public void setNormalImageURL(URL normalImageURL)
+	{
+		this.normalImageURL = normalImageURL;
+	}
+
+	public Manufacturer getManufacturer()
+	{
+		return manufacturer;
+	}
+
+	public void setManufacturer(Manufacturer manufacturer)
+	{
+		this.manufacturer = manufacturer;
+	}
+
+	public Engine getEngine()
+	{
+		return engine;
+	}
+
+	public void setEngine(Engine engine)
+	{
+		this.engine = engine;
+	}
+
+	public Seat getSeat()
+	{
+		return seat;
+	}
+
+	public void setSeat(Seat seat)
+	{
+		this.seat = seat;
+	}
 
 }
